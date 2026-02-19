eb8949f78a298504b2dd23f3791970ff1204fa9e
Grebennikov Roman <grv@dfdx.me>
2026-02-19 14:52:53 +0100

    implement column and string column

--- src/table/mod.rs
+++ src/table/mod.rs
@@ -1,4 +1,8 @@
+pub mod column;
+pub mod dense_string;
 mod table;
 
+pub use column::{Column, SegmentIndex};
+pub use dense_string::DenseStringColumn;
 pub use table::KeyIndex;
 pub use table::Table;
