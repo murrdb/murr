1ce835f079fda11821b37a59584458b1480f89fa
Grebennikov Roman <grv@dfdx.me>
2026-02-26 18:33:07 +0100

    fix bugs

--- python/python/murr/__init__.py
+++ python/python/murr/__init__.py
@@ -1,4 +1,12 @@
 from murr.client import LocalMurr
+from murr.libmurr import MurrSegmentError, MurrTableError
 from murr.schema import ColumnSchema, DType, TableSchema
 
-__all__ = ["DType", "ColumnSchema", "TableSchema", "LocalMurr"]
+__all__ = [
+    "DType",
+    "ColumnSchema",
+    "TableSchema",
+    "LocalMurr",
+    "MurrTableError",
+    "MurrSegmentError",
+]
